19b710660c6f89e685704523095075a30e588392
fajrulfalah18 <fajrul1836@gmail.com>
2026-06-05 10:49:35 +0700

    pertemuan 12

---
+++ pertemuan12/add_edge.py
@@ -0,0 +1,19 @@
+class graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def print_graph(self):
+        print(self.graph)
+
+g = graph()
+g.add_edge("A", "B")
+g.add_edge("A", "C")
+g.print_graph()
---
+++ pertemuan12/add_vertax.py
@@ -0,0 +1,15 @@
+class graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_vertex(self, vertex):
+        if vertex not in self.graph:
+            self.graph[vertex] = []
+
+    def print_graph(self):
+        print(self.graph)
+
+g = graph()
+g.add_vertex("A")
+g.add_vertex("B")
+g.print_graph()
